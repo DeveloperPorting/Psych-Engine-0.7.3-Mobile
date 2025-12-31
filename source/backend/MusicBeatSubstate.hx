289d5aa1be5bcfc5ce7363fc4ea88e17e99e1332
StarNovaBR <creambr6@gmail.com>
2025-12-31 13:06:47 -0300

    mds

--- source/backend/MusicBeatSubstate.hx
+++ source/backend/MusicBeatSubstate.hx
@@ -4,8 +4,10 @@ import flixel.FlxSubState;
 
 class MusicBeatSubstate extends FlxSubState
 {
+    public static var instance:MusicBeatSubstate;
 	public function new()
 	{
+	    instance = this;
 		super();
 	}
 
