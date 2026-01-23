8196ccd22a03692ea0afdf73f6d6bbf68a0c8f73
justaguy6 <outerjose@gmail.com>
2026-01-23 10:39:11 -0300

    oh

--- source/mobile/backend/StorageSystem.hx
+++ source/mobile/backend/StorageSystem.hx
@@ -18,15 +18,15 @@ import haxe.io.Bytes;
 using StringTools;
 
 /** 
-* @Authors StarNova (Cream.BR)
-* @version: 0.1.2
+ * @Authors StarNova (Cream.BR)
+ * @version: 0.1.2
 **/
+class StorageSystem
+{
+	public static inline function getStorageDirectory():String
+		return #if android Path.addTrailingSlash(Environment.getExternalStorageDirectory() + '/.' +
+			Application.current.meta.get('file')) #elseif ios lime.system.System.documentsDirectory #else Sys.getCwd() #end;
 
-class StorageSystem {
-  
-    public static inline function getStorageDirectory():String
-	  	return #if android Path.addTrailingSlash(Environment.getExternalStorageDirectory() + '/.' + Application.current.meta.get('file')) #elseif ios lime.system.System.documentsDirectory #else Sys.getCwd() #end;
-	
 	public static function getDirectory():String
 	{
 		#if android
@@ -39,107 +39,138 @@ class StorageSystem {
 	}
 
 	/**
-     * Request permission to access the files, only if they are not already granted.
-     */
-    public static function getPermissions():Void {
-        #if android
-        try {
-        var needsPermissions:Bool = false;
-        var sdk:Int = VERSION.SDK_INT;
-        
-        var granted:Array<String> = Permissions.getGrantedPermissions();
-    
-        if (sdk >= VERSION_CODES.TIRAMISU) {
-            // Android 13+
-            if (!granted.contains('READ_MEDIA_IMAGES') || !granted.contains('READ_MEDIA_AUDIO')) {
-                needsPermissions = true;
-                Permissions.requestPermissions(['READ_MEDIA_IMAGES', 'READ_MEDIA_VIDEO', 'READ_MEDIA_AUDIO', 'READ_MEDIA_VISUAL_USER_SELECTED']);
-            }
-        } else {
-            // Android 12 and lower
-            if (!granted.contains('WRITE_EXTERNAL_STORAGE')) {
-                needsPermissions = true;
-                Permissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
-            }
-        }
+	 * Request permission to access the files, only if they are not already granted.
+	 */
+	public static function getPermissions():Void
+	{
+		#if android
+		try
+		{
+			var needsPermissions:Bool = false;
+			var sdk:Int = VERSION.SDK_INT;
 
-        if (sdk >= VERSION_CODES.R) { 
-            if (!Environment.isExternalStorageManager()) {
-                needsPermissions = true;
-                Settings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
-            }
-        }
+			var granted:Array<String> = Permissions.getGrantedPermissions();
 
-        if (needsPermissions) {
-            Tools.showAlertDialog("Requires permissions", "Please allow the necessary permissions to play.\nPress OK & let's see what happens", {name: "OK", func: null}, null);
-        } else {
-            trace("All permissions already granted.");
-        }
-        } catch (e:Dynamic) {
-  	      trace('Erro ao pedir permissãos: $e');
-        }
-        #else
-        trace("Permissions request not required or not implemented for this platform.");
-        #end
-    }
+			if (sdk >= VERSION_CODES.TIRAMISU)
+			{
+				// Android 13+
+				if (!granted.contains('READ_MEDIA_IMAGES') || !granted.contains('READ_MEDIA_AUDIO'))
+				{
+					needsPermissions = true;
+					Permissions.requestPermissions([
+						'READ_MEDIA_IMAGES',
+						'READ_MEDIA_VIDEO',
+						'READ_MEDIA_AUDIO',
+						'READ_MEDIA_VISUAL_USER_SELECTED'
+					]);
+				}
+			}
+			else
+			{
+				// Android 12 and lower
+				if (!granted.contains('WRITE_EXTERNAL_STORAGE'))
+				{
+					needsPermissions = true;
+					Permissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
+				}
+			}
 
-	public static function copyAssetsToStorage(sources:Array<String>, targetPath:String = null):Void {
-	    #if mobile
-	    try {
-    	    var baseDest = (targetPath == null) ? getDirectory() : targetPath;
-        
-  	      var allExists = true;
-   	     for (source in sources) {
-        	    var checkPath = Path.join([baseDest, source]);
-      	      if (!FileSystem.exists(checkPath)) {
-           	     allExists = false;
-         	       break;
-         	   }
-   	     }
+			if (sdk >= VERSION_CODES.R)
+			{
+				if (!Environment.isExternalStorageManager())
+				{
+					needsPermissions = true;
+					Settings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
+				}
+			}
 
-     	   if (allExists) {
-        	    trace("Assets já existem no destino. Pulando cópia.");
-        	    return;
-    	    }
+			if (needsPermissions)
+			{
+				Tools.showAlertDialog("Requires permissions", "Please allow the necessary permissions to play.\nPress OK & let's see what happens",
+					{name: "OK", func: null}, null);
+			}
+			else
+			{
+				trace("All permissions already granted.");
+			}
+		}
+		catch (e:Dynamic)
+		{
+			trace('Erro ao Solicitar Permissoes: $e');
+		}
+		#else
+		trace("Permissions request not required or not implemented for this platform.");
+		#end
+	}
 
-     	   var assetList:Array<String> = Assets.list();
-   	     var filesCopied:Int = 0;
+	public static function copyAssetsToStorage(sources:Array<String>, targetPath:String = null):Void
+	{
+		#if mobile
+		try
+		{
+			var baseDest = (targetPath == null) ? getDirectory() : targetPath;
 
-   	     for (source in sources) {
-        	    var filter = Path.normalize(source);
-      	      if (StringTools.endsWith(filter, "/")) {
-       	         filter = filter.substring(0, filter.length - 1);
-       	     }
+			if (baseDest == null)
+			{
+				trace("Erro: Diretório de destino é nulo!");
+				return;
+			}
 
-        	    trace('Sincronizando origem: $filter');
+			var assetList:Array<String> = Assets.list();
+			var filesCopied:Int = 0;
 
-      	      for (assetPath in assetList) {
-         	       if (StringTools.startsWith(assetPath, filter)) {
-                    
-               	     var fullTargetPath = Path.normalize(Path.join([baseDest, assetPath]));
-            	        var directory = Path.directory(fullTargetPath);
+			for (source in sources)
+			{
+				var filter = Path.normalize(source);
 
-              	      if (!FileSystem.exists(directory)) {
-              	          FileSystem.createDirectory(directory);
-           	         }
+				if (StringTools.endsWith(filter, "/"))
+				{
+					filter = filter.substring(0, filter.length - 1);
+				}
 
-            	        if (Assets.exists(assetPath) && !FileSystem.exists(fullTargetPath)) {
-               	         var data:Bytes = Assets.getBytes(assetPath);
-                 	       if (data != null) {
-                   	         File.saveBytes(fullTargetPath, data);
-                     	       filesCopied++;
-                 	       }
-             	       }
-          	      }
-    	        }
-   	     }
-        
-     	   trace('Operação finalizada. Arquivos novos: $filesCopied');
+				trace('Sincronizando origem: $filter');
 
-	    } catch (e:Dynamic) {
-  	      trace('Erro ao sincronizar pastas: $e');
-   	     Application.current.window.alert('Error', 'Failed to copy files: ' + e);
- 	   }
-   	 #end
+				for (assetPath in assetList)
+				{
+					if (StringTools.startsWith(assetPath, filter) && !StringTools.startsWith(assetPath, "manifest"))
+					{
+						var fullTargetPath = Path.normalize(Path.join([baseDest, assetPath]));
+						var directory = Path.directory(fullTargetPath);
+
+						if (assetPath == filter || assetPath == filter + "/")
+							continue;
+
+						if (!FileSystem.exists(directory))
+						{
+							FileSystem.createDirectory(directory);
+						}
+
+						if (!FileSystem.exists(fullTargetPath))
+						{
+							try
+							{
+								var data:Bytes = Assets.getBytes(assetPath);
+
+								if (data != null)
+								{
+									File.saveBytes(fullTargetPath, data);
+									filesCopied++;
+								}
+							}
+							catch (e:Dynamic)
+							{
+								continue;
+							}
+						}
+					}
+				}
+			}
+			trace('Operação finalizada. Arquivos novos: $filesCopied');
+		}
+		catch (e:Dynamic)
+		{
+			trace('Erro fatal na sincronização: $e');
+		}
+		#end
 	}
 }
